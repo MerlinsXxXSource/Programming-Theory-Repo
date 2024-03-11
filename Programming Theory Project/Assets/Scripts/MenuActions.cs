741a8f2a3a441997ef228ebb832571c12ebcfed6
MerlinsXxXSource <156229726+MerlinsXxXSource@users.noreply.github.com>
2024-03-11 14:42:25 -0500

    title screen
    * added a title.
    * added a play button with functionality.
    * added a quit button with functionality.
    * added a sound button to toggle sound on/off.
    
    --The title screen should be finished.

---
+++ Programming Theory Project/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    // Call this method when the Play button is clicked
+    public void PlayGame()
+    {
+        // Replace "MainScene" with the name of your main scene
+        SceneManager.LoadScene("Main");
+    }
+
+    // Call this method when the Quit button is clicked
+    public void QuitGame()
+    {
+        // This will close the application
+        Application.Quit();
+    }
+
+    // Call this method when the Options button is clicked
+    public void ToggleSound()
+    {
+        // This will toggle the sound volume between 0 (off) and 1 (on)
+        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+    }
+}
